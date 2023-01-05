29ea1d054d3f273175942e6b1302e95fec5fd8f5
Tiara Silva <tsilva7@uc.cl>
2023-01-05 16:35:20 -0300

    agregar los valores de la venta actual

--- Internacional/generar_rango_proyeccion.py
+++ Internacional/generar_rango_proyeccion.py
@@ -31,15 +31,36 @@ wb = Workbook()
 
 for office in dict_parametros_venta[tipo_venta.lower()]:
   ws = wb.create_sheet(office)
-  ws.append(['Sector', 'Material', 'Venta plan', 'Stock planta', 'Puerto Chile', 'Centro Agua', 'Puerto Oficina', 'Almacen oficina','Pesimista Proy.', 'Optimista. Proy.'])
+  print(wb.sheetnames)
+  ws.append(['Sector', 'Material', 'Descripción', 'Venta plan', 'Stock planta', 'Puerto Chile', 'Centro Agua', 'Puerto Oficina', 'Almacen oficina','Pesimista Proy.', 'Optimista. Proy.'])
   run_styles(ws)
 
-# ----- 3. Leemos Venta actual
-wb_venta = load_workbook(filename_venta, data_only = True, read_only = True)
-ws_venta = wb['Venta - Plan']
+del wb['Sheet']
 
-for row in ws_venta.iter_rows(2, ws_venta.max_row, values_only=True):
-  print(row)
+# ----- 3. Leemos Venta actual
+wb_venta = load_workbook(filename_venta, data_only=True, read_only=True)
+ws_venta = wb_venta['Venta - Plan']
+
+for row in ws_venta.iter_rows(7, ws_venta.max_row, values_only=True):
+  sector = row[1]
+  material = row[2]
+  descripcion = row[3]
+  oficina = row[5]
+  plan_total = row[6]
+  venta_total = row[7]
+  if oficina in wb.sheetnames:
+    ws = wb[oficina]
+    ws.append({1: sector, 2: material, 3: descripcion, 4: venta_total})
+
+
+# ----- 4. Puerto Chile, Centro Agua, Puerto Oficina, Almacén Oficina
+wb_puerto = load_workbook(filename_puerto, data_only=True, read_only=True)
+ws_puerto = wb_puerto.active
+
+# for row in ws_puerto.iter_rows(7, ws_puerto.max_row, values_only=True):
+
+
+# ----- 5. Stock planta
 
 wb.save(filename)
 wb.close()
